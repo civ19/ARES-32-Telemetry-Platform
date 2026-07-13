fe5c527c7cb62925027b65a334859d2688c6c0c1
Ronaldo <ronaldoanthony12@gmail.com>
2026-07-13 16:46:22 -0400

    application.properties fix

--- weather-api/src/main/java/com/sensor/weatherapi/SensorController.java
+++ weather-api/src/main/java/com/sensor/weatherapi/SensorController.java
@@ -1,12 +1,21 @@
 package com.sensor.weatherapi;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 
+import java.util.List;
+
 @RestController
+@RequiredArgsConstructor
 public class SensorController {
 
-    @GetMapping("/weather/data")
-    public ResponseEntity<SensorResponse> getReadings
+    private final SensorService service;
+    @GetMapping("/api/sensors")
+    public ResponseEntity<List<SensorResponse>> getReadings() {
+        List<SensorResponse> resp = service.getAll24();
+        return new ResponseEntity<List<SensorResponse>>(resp, HttpStatus.OK);
+    }
 }
