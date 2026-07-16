7b26b57c05c2bacd4b55263f976663f0c94ee906
Ronaldo <ronaldoanthony12@gmail.com>
2026-07-16 14:35:47 -0400

    BLE section 2

--- src/nimBLE/ble_prov.c
+++ src/nimBLE/ble_prov.c
@@ -1,5 +1,39 @@
 #include "host/ble_hs.h"
+#include "services/gatt/ble_svc_gatt.h"
 
 
-static const ble_uuid128_t prov_service_uuid = BLE_UUID128_DECLARE(0x2d, 0x71, 0xa1, 0x20, 0x53, 0x75, 0x49, 0x73, 
-                        0xad, 0x57, 0x07, 0x72, 0xab, 0x39, 0x10, 0x11);
+static int gatt_svr_access_cb(uint16_t conn_handle, uint16_t attr_handle, 
+    struct ble_gatt_access_ctxt *ctxt, void* arg);
+
+
+static const struct ble_gatt_svc_def gatt_svr_svcs[] = {
+    {
+        .type = BLE_GATT_SVC_TYPE_PRIMARY,
+        .uuid = BLE_UUID128_DECLARE(0x2d, 0x71, 0xa1, 0x20, 0x53, 0x75, 0x49, 0x73, //prov service
+                        0xad, 0x57, 0x07, 0x72, 0xab, 0x39, 0x10, 0x11),
+        
+        .characteristics = prov_features,
+    },
+    {.type = BLE_GATT_SVC_TYPE_END}
+
+};
+
+static const struct ble_gatt_chr_def prov_features[] = {
+    { //ssid
+        .uuid = BLE_UUID128_DECLARE(0x2d, 0x71, 0xa1, 0x20, 0x53, 0x75, 0x49, 0x73, 
+                        0xad, 0x57, 0x07, 0x72, 0xab, 0x39, 0x10, 0x12),
+        .access_cb = gatt_svr_access_cb,
+            .flags = BLE_GATT_CHR_F_WRITE,
+    },
+
+
+    { //pass
+        .uuid = BLE_UUID128_DECLARE(0x2d, 0x71, 0xa1, 0x20, 0x53, 0x75, 0x49, 0x73, 
+                        0xad, 0x57, 0x07, 0x72, 0xab, 0x39, 0x10, 0x13),
+        .access_cb = gatt_svr_access_cb,
+        .flags = BLE_GATT_CHR_F_WRITE,
+    },
+    {.uuid = NULL} //signla to stop processing
+
+};
+
